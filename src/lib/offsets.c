9d9b9ca153930e3fdaf4a79cb760ca757b35aa78
Siguza <siguza@siguza.net>
2017-02-12 16:58:51 +0100

    Extend offsets registry for models known to share the same kernel

--- src/lib/offsets.c
+++ src/lib/offsets.c
@@ -33,14 +33,18 @@ static addr_t reg_anchor(void)
         case M_N51AP  | V_13A452:
         case M_N53AP  | V_13A452:
         case M_N66AP  | V_13A452:
+        case M_N66mAP | V_13A342:
+        case M_N66mAP | V_13A405:
         case M_N66mAP | V_13A452:
         case M_N71AP  | V_13A452:
         case M_N71mAP | V_13A342:
         case M_N71mAP | V_13A405:
         case M_N71mAP | V_13A452:
             return 0xffffff800453e000;
+        case M_N56AP  | V_13G34:
         case M_N61AP  | V_13G34:
             return 0xffffff8004542000;
+        case M_N66AP  | V_13G34:
         case M_N69AP  | V_13G34:
         case M_N71AP  | V_13G34:
             return 0xffffff8004536000;
@@ -73,14 +77,18 @@ static addr_t reg_vtab(void)
         case M_N51AP  | V_13A452:
         case M_N53AP  | V_13A452:
         case M_N66AP  | V_13A452:
+        case M_N66mAP | V_13A342:
+        case M_N66mAP | V_13A405:
         case M_N66mAP | V_13A452:
         case M_N71AP  | V_13A452:
         case M_N71mAP | V_13A342:
         case M_N71mAP | V_13A405:
         case M_N71mAP | V_13A452:
             return 0xffffff80044f7168;
+        case M_N56AP  | V_13G34:
         case M_N61AP  | V_13G34:
             return 0xffffff80044fb1f0;
+        case M_N66AP  | V_13G34:
         case M_N69AP  | V_13G34:
         case M_N71AP  | V_13G34:
             return 0xffffff80044ef1f0;
